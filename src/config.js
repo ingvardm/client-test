d880079395b4f63fc67a0c6b2669441336106d1e
igor poughtach <ingvardm@gmail.com>
2018-02-27 20:05:33 +0200

    export env vars

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
+import Config from 'react-native-config'
+
 export const FACEBOOK_API = 'https://graph.facebook.com/v2.5/me?fields=name,picture&access_token='
+export const GOOGLE_WEBCLIENT_ID = Config.GOOGLE_WEBCLIENT_ID
