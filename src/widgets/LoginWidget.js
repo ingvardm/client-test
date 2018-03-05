5c7ec039f2f83f7227239cd9456a305b6bdb193c
igor poughtach <ingvardm@gmail.com>
2018-03-05 10:51:41 +0200

    remove empty lines

--- src/widgets/LoginWidget.js
+++ src/widgets/LoginWidget.js
@@ -103,10 +103,8 @@ export default class LoginWidget extends Component {
     onHandleRelease = async (e, gesture) => {
         if(this.handleLocked) return
         let signedIn = false
-        
         if(gesture.dx > triggerOffset) signedIn = await this.googleAuth().catch(e => console.log(e))
         else if(gesture.dx < -triggerOffset) signedIn = await this.facebookAuth()
-        
         if(!signedIn) this.reset()
     }
 
